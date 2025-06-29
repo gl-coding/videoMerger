959ea85a5f00569dda6d9123b50433820c281d93
gl-coding <cool3stone@gmail.com>
2025-06-29 11:43:27 +0800

    fix

--- x_run.sh
+++ x_run.sh
@@ -1,3 +1,13 @@
-python video_merger.py image.jpeg qinghuanv.wav output.mp4 30 30 qinghuanv.srt
+filename=data/qinghuanv
+tmp_file=data/tmp.mp4
 
-#ffmpeg -i output.mp4 -vf "subtitles=qinghuanv.srt" output1.mp4
+#视频合成
+python video_merger.py $filename.jpeg $filename.wav $tmp_file
+
+#语音转字幕
+python srt_gen.py $filename.wav $filename.srt
+
+#字幕合成
+ffmpeg -i $tmp_file -vf "subtitles=$filename.srt" $filename.mp4
+
+rm $tmp_file
